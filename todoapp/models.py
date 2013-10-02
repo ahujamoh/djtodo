21c7bcb9ff5cbc47b1653f2b888c9576e1f8bfbc
iamsudip <iamsudip@programmer.net>
2013-10-03 00:36:03 +0530

    Fixed docstring indentation problem

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class TodoList(models.Model):
-'''
-    Table in database as title, description and datetime for todo list
-'''
+    '''
+        Table in database as title, description and datetime for todo list
+    '''
     title = models.CharField(max_length=200, unique=True) # Title for to-do list
     description = models.TextField() # Description field for todo list
     datetime = models.DateTimeField() # Creation date tiome
   
     def __unicode__(self):
-    '''
-    Returns title of the todo task
-    '''
+        '''
+            REturns todolist title
+        '''
         return self.title
