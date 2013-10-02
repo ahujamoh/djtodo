57bc5e0ce8e828c79cd2d361f8a64d98af7f24a7
iamsudip <iamsudip@programmer.net>
2013-10-03 00:21:02 +0530

    Activated admin and added todoapp to installed apps

--- djtodo/settings.py
+++ djtodo/settings.py
@@ -120,10 +120,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'todoapp',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
