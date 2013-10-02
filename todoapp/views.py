15f17ee43bd6a87083b43e10cdf577bed2abbd36
iamsudip <iamsudip@programmer.net>
2013-10-03 02:07:36 +0530

    Added home() to response request

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,2 +1,9 @@
-def home():
-    pass
+from models import TodoList 
+from django.shortcuts import render_to_response
+
+def home(request):
+    '''
+        Returns all the to-do entries with all informaion to view at /home
+    '''
+    todos = TodoList.objects.all()
+    return render_to_response('home.html', {'todos': todos})
