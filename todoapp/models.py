743720b663e9950de13d600611d7ff499d2e18aa
iamsudip <iamsudip@programmer.net>
2013-10-03 00:28:07 +0530

    Added database tables for todolst

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class TodoList(models.Model):
+'''
+    Table in database as title, description and datetime for todo list
+'''
+    title = models.CharField(max_length=200, unique=True) # Title for to-do list
+    description = models.TextField() # Description field for todo list
+    datetime = models.DateTimeField() # Creation date tiome
+  
+    def __unicode__(self):
+'''
+    Returns title of the todo task
+'''
+        return self.title
