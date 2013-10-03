005bc8d42124e7ba78f93269999a716b5cd7360a
iamsudip <iamsudip@programmer.net>
2013-10-03 11:07:09 +0530

    Added priority and done field

--- todoapp/models.py
+++ todoapp/models.py
@@ -5,8 +5,10 @@ class TodoList(models.Model):
         Table in database as title, description and datetime for todo list
     '''
     title = models.CharField(max_length=200, unique=True) # Title for to-do list
-    description = models.TextField() # Description field for todo list
-    datetime = models.DateTimeField() # Creation date tiome
+    datetime = models.DateTimeField()                     # Creation date tiome
+    priority = models.IntegerField(default=0)             # Priority of the task
+    done = models.BooleanField(default=False)             # Done or not!
+    description = models.TextField()                      # Description field for todo list
   
     def __unicode__(self):
         '''
