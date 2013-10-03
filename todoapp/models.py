6d3417fc7e45bba12e5015ef9846c3e659b69d05
iamsudip <iamsudip@programmer.net>
2013-10-03 14:34:04 +0530

    Versbose task status and priority

--- todoapp/models.py
+++ todoapp/models.py
@@ -4,10 +4,19 @@ class TodoList(models.Model):
     '''
         Table in database as title, description and datetime for todo list
     '''
-    title = models.CharField(max_length=200, unique=True) # Title for to-do list
-    datetime = models.DateTimeField()                     # Creation date tiome
-    priority = models.IntegerField(default=0)             # Priority of the task
-    state = models.BooleanField(default=False)             # Done or not!
+    priority_tuple = (
+        ('Little', 'Little'),
+        ('Normal', 'Normal'),
+        ('Important', 'Important'),
+    )
+    status_tuple = (
+        ('Done', 'Done'),
+        ('Pending', 'Pending'),
+    )
+    title = models.CharField(max_length=200)                                 # Title for to-do list
+    datetime = models.DateTimeField()                                        # Creation date tiome
+    priority = models.CharField(max_length=10, choices=priority_tuple, default='Normal')        # Priority of the task
+    status = models.CharField(max_length=10, choices=status_tuple, default='Pending')           # Done or not!
   
     def __unicode__(self):
         '''
