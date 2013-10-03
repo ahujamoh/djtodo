8d850fa48c87dcfd89adfecd2b35daa8b3c0c760
iamsudip <iamsudip@programmer.net>
2013-10-03 11:25:30 +0530

    REmoved time wasting option description

--- todoapp/models.py
+++ todoapp/models.py
@@ -7,8 +7,7 @@ class TodoList(models.Model):
     title = models.CharField(max_length=200, unique=True) # Title for to-do list
     datetime = models.DateTimeField()                     # Creation date tiome
     priority = models.IntegerField(default=0)             # Priority of the task
-    done = models.BooleanField(default=False)             # Done or not!
-    description = models.TextField()                      # Description field for todo list
+    state = models.BooleanField(default=False)             # Done or not!
   
     def __unicode__(self):
         '''
