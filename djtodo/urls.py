4adb130bae6bcb9ddd82be35e1c5641dccefed7d
iamsudip <iamsudip@programmer.net>
2013-10-04 20:52:14 +0530

    Added search url

--- djtodo/urls.py
+++ djtodo/urls.py
@@ -5,6 +5,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'todoapp.views.home', name='home'),
     url(r'^stats', 'todoapp.views.stats', name='stats'),
+    url(r'^search', 'todoapp.views.search', name='search results'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
