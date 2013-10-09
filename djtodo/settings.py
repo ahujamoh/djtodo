d2307218c412e1c083ef2342c29844321fa5ad88
iamsudip <iamsudip@programmer.net>
2013-10-09 11:26:59 +0530

    Moved templates to templates dir

--- djtodo/settings.py
+++ djtodo/settings.py
@@ -109,7 +109,8 @@ ROOT_URLCONF = 'djtodo.urls'
 WSGI_APPLICATION = 'djtodo.wsgi.application'
 
 TEMPLATE_DIRS = (
-    ''
+    '',
+    'templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
